7ac8f15eb34a8c2965ba3df6df7193e2fab98b4a
Manishi123 <manishisaxena1999@gmail.com>
2020-05-17 20:36:06 +0530

    changes made

--- app/src/models/compute.py
+++ app/src/models/compute.py
@@ -21,6 +21,20 @@ class Compute:
     def ackermann(m, n):
         return
 
-
+    @staticmethod
     def factorial(n):
-        return
+        is_negative = n < 0
+        if is_negative:
+            n = -1*n
+        if n >= 0 and n <= 1:
+            return n
+
+        fib = [None]*(n + 1)
+        fib[0] = 0
+        fib[1] = 1
+
+        for i in range(2, n+1):
+            if not fib[i]:
+                fib[i] = fib[i-1] * i
+
+        return pow(-1, n+1)*fib[n] if is_negative else fib[n]
